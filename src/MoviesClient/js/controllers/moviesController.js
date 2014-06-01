fec7f2adca671ea631915550edf3129102ef0634
Sergey Sviridov <sviridovserg@gmail.com>
2014-06-01 19:37:20 +0400

    Fix deprecated unauthorized actions

--- src/MoviesClient/js/controllers/moviesController.js
+++ src/MoviesClient/js/controllers/moviesController.js
@@ -1,17 +1,23 @@
 ﻿controllers.controller('moviesController', function ($scope, $location, moviesService) {
-	moviesService.getMovies().then(function (movies) {
-		$scope.movies = movies;
-	});
+    moviesService.getMovies().then(function (movies) {
+        $scope.movies = movies;
+    });
 
-	$scope.addMovie = function () {
-		$location.path('/movies/add')
-	}
+    $scope.addMovie = function () {
+        if (!$scope.loggedIn) {
+            return;
+        }
+        $location.path('/movies/add')
+    }
 
-	$scope.editMovie = function (movie) {
-		$location.path('/movies/update/' + movie.cacheId);
-	}
+    $scope.editMovie = function (movie) {
+        if (!$scope.loggedIn) {
+            return;
+        }
+        $location.path('/movies/update/' + movie.cacheId);
+    }
 
-	$scope.fields = [
+    $scope.fields = [
         { id: "id", value: "Id" },
         { id: "title", value: "Title" },
         { id: "releaseYear", value: "Year" },
@@ -21,27 +27,27 @@
         { id: "cast", value: "Actor" },
     ];
 
-	$scope.search = function (field, expression) {
-		if (!field || !expression) {
-			alert('Search field and expression should be entered');
-		}
-		moviesService.searchMovies(field.id, expression).then(function (movies) {
-			$scope.movies = movies;
-		});
-	};
+    $scope.search = function (field, expression) {
+        if (!field || !expression) {
+            alert('Search field and expression should be entered');
+        }
+        moviesService.searchMovies(field.id, expression).then(function (movies) {
+            $scope.movies = movies;
+        });
+    };
 
-	$scope.sort = function (field, asc) {
-		if ($scope.sortField != field) {
-			$scope.asc = undefined;
-		}
-		$scope.sortField = field;
-		if ($scope.asc == undefined) {
-			$scope.asc = false;
-		} else if (!$scope.asc) {
-			$scope.asc = true;
-		} else {
-			$scope.asc = undefined;
-			$scope.sortField = undefined;
-		}
-	}
+    $scope.sort = function (field, asc) {
+        if ($scope.sortField != field) {
+            $scope.asc = undefined;
+        }
+        $scope.sortField = field;
+        if ($scope.asc == undefined) {
+            $scope.asc = false;
+        } else if (!$scope.asc) {
+            $scope.asc = true;
+        } else {
+            $scope.asc = undefined;
+            $scope.sortField = undefined;
+        }
+    }
 });
